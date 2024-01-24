fe8cd0fb1fba813feb1ded1cca7ce4d0b77e3c16
Armikrog10 <76245740+Armikrog10@users.noreply.github.com>
2024-01-24 19:31:28 +0300

    Add files via upload

---
+++ Пр№1.swift
@@ -0,0 +1,28 @@
+var greting = "Привет"
+var name = "Мир!"
+var massege = greting + " " + name + "\n"
+print(massege)
+
+for i in 1...5
+{
+    print(i)
+}
+
+print()
+
+var Vova = 0;
+
+while Vova < 5
+{
+    Vova+=1
+    if(Vova < 3)
+    {
+        print("Вова меньше трёх")
+    }
+    else
+    {
+        print("Вова уже не меньше трёх")
+    }
+}
+
+
